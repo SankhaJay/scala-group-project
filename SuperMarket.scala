6857d146ba0f811e555ab21bed4e6ebea5d785da
Kiruthiharan <kiruthiharan@gmail.com>
2019-03-09 19:15:01 +0530

    class created and quantity deduction added

--- SuperMarket.scala
+++ SuperMarket.scala
@@ -1,12 +1,23 @@
 object SuperMarket{
-    class Item(var price:Int,var qty:Int){
-        var price:Int =price;
-        var qty:Int=qty;
-        var name:String=name;
+    class Item(var icode:Int, var iprice:Int,var iqty:Int, var iname:String){
+        var code:Int=icode;
+        var price:Int =iprice;
+        var qty:Int=iqty;
+        var name:String=iname;
+
+        def UpdateItem(icode:Int, iqty:Int): Unit={
+            qty=qty-iqty;
+        }
     }
     def main(args: Array[String]){
-        println("gi");
-        val item1=new Item(5);
-        println(item1.price);
+        println("###Super Market###");
+        val item1=new Item(1,500,10,"Soap");
+        println(item1.price+" "+ item1.qty+" "+item1.name+"\n");
+
+        var code=readLine("Enter item code: ");
+        var qty=readLine("Enter Quantity: ");
+        item1.UpdateItem(code.toInt,qty.toInt)
+        println(item1.price+" "+ item1.qty+" "+item1.name+"\n");
+
     }
 }
