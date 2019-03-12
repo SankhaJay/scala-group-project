07e283d4fbab7c65d31ac443990e18a72031130d
SankhaJay <sankha.rc@gmail.com>
2019-03-12 22:33:41 +0530

    completed itemList function

--- SuperMarket.scala
+++ SuperMarket.scala
@@ -1,18 +1,18 @@
 object SuperMarket{
     var inum:Int =0;
     var cnum:Int =0;
-    
+    val N:Int = 10;
     class Item(var icode:Int, var iprice:Int,var iqty:Int, var iname:String){
         var code:Int=icode;
         var price:Int =iprice;
         var qty:Int=iqty;
         var name:String=iname;
 
-        def UpdateItem(icode:Int, iqty:Int): Unit={
+        /*def UpdateItem(icode:Int, iqty:Int): Unit={
             qty=qty-iqty;
-        }
+        }*/
     }
-    val items: Array[Item] = new Array[Item](10);
+    val items: Array[Item] = new Array[Item](N);
 
     def main(args: Array[String]){
         //println(item1.price+" "+ item1.qty+" "+item1.name+"\n");
@@ -30,34 +30,57 @@ object SuperMarket{
         do{
             println("Enter choice");
             println("01.Add a new item");
-            println("02.Proceed");
+            println("02.Proceed to the shop");
+            println("03.Exit");
             op = scala.io.StdIn.readInt();
             op match{
             case 1 => addNewItem();
             case 2 => enterShop();
+            case 3 => System.exit(1);
             case _ => println("Enter a valid choice");
             }
         }while(op == 1);
     }
     def addNewItem(){
+        if(inum == N){
+            println("Cannot add more items. Please proceed or exit\n");
+            return;
+        }
         println("Enter item name");
         var iname = scala.io.StdIn.readLine();
         println("Enter unit price");
         var iprice = scala.io.StdIn.readInt();
         println("Enter remaining item quantity");
         var iqty = scala.io.StdIn.readInt();
-        val temp = new Item(inum,iprice,iqty,iname);
-        items(inum) = temp;
         inum = inum + 1;
-        println("An item added succesfully");
-        println(" ");
+        val temp = new Item(inum,iprice,iqty,iname);
+        items(inum-1) = temp;
+        
+        println("An item added succesfully\n");
     }
     def closeShop(){
         println("Shop is closed by the owner!!");
         System.exit(1);
     }
+    def buyItem(){
+        println("Enter item code");
+        var icode = scala.io.StdIn.readInt();
+        println("Enter quantity to buy");
+        var iqty = scala.io.StdIn.readInt();
+    }
+    def itemList(){
+        var i:Int = 0;
+        println("\n------Item list------")
+        for( i <- 0 to inum-1){
+            println(items(i).icode+"-->"+items(i).iname);
+        }
+    }
     def newCustomer(){
-        println("This is a new customer");
+        cnum = cnum + 1;
+        itemList();
+        println("Enter choice");
+        println("01.Buy items");
+        println("02.Issue Bill");
     }
     def enterShop(){
         println("###Super Market###");
